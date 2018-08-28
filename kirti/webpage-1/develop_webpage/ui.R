dca52e10defe2437fc8acf1e8234ba16f868e314
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2018-08-27 21:46:00 -0700

    hey

--- kirti/webpage-1/develop_webpage/ui.R
+++ kirti/webpage-1/develop_webpage/ui.R
@@ -2,6 +2,8 @@ library(ggplot2)
 library(shiny)
 
 ui = navbarPage("Hydro Lab", inverse=TRUE, collapsable=TRUE,
+                
+                # Home Tab starts here
                 tabPanel(title = "Home",
                          fluidRow(
                            column(5, offset = 0,  
@@ -13,24 +15,28 @@ ui = navbarPage("Hydro Lab", inverse=TRUE, collapsable=TRUE,
                                    tabPanel(title = "What's the Story", br()),
                                    tabPanel(title = "Contact"),
                                    tabPanel(title = "Take a Tour (video)")
+                                              ) # navlistPanel ends here
                                  )
                                 )
-                         )
-                         ),
+                         ), # tabPanel of Home Page ends here
+                
                 navbarMenu(title = "Bloom",
                            tabPanel(title = "Median Day of Year"),
                            tabPanel(title = "Difference from Historical")),
+                
+                
                 navbarMenu(title = "CM Flight",
                            tabPanel(title = "Median Day of Year (1st Flight)"),
                            tabPanel(title = "Difference from Historical (1st Flight)"),
                            tabPanel(title = "Median Day of Year (By Generation)"),
                            tabPanel(title = "Difference from Historical (By Generation)")
-                ),
+                           ),
+                
                 navbarMenu(title = "CM Egg Hatch",
                            tabPanel(title = "Pest Risk"),
                            tabPanel(title = "Median Day of Year (By Generation)"),
                            tabPanel(title = "Difference from Historical (By Generation)")
-                ),
+                           ),
                 
                 tabPanel(title = "CM Diapause"),
                 tabPanel(title = "Regional Plots"),
@@ -43,7 +49,7 @@ ui = navbarPage("Hydro Lab", inverse=TRUE, collapsable=TRUE,
                                textInput(inputId = "box_1", label = "Enter Tab 1 Text:", value="Tab 1!"),
                                textInput(inputId = "box_2", label = "Enter Tab 2 Text:", value="Tab 2!"),
                                textInput(inputId = "box_3", label = "Enter Tab 3 Text:", value="Tab 3!")
-                             ),
+                                          ),
                              
                              mainPanel(
                                tabsetPanel(type="tabs",
@@ -55,13 +61,16 @@ ui = navbarPage("Hydro Lab", inverse=TRUE, collapsable=TRUE,
                                         )
                                      ) 
                          ),
+                
+                ## Leaflet test tab
                 tabPanel(title = "Leaflet",
                          fluidPage(leafletOutput("mymap"), 
                                    p(), 
                                    actionButton("recalc", "New points")
                                    )
                          )
-)
+
+                ) # navbarPage Ends here
 
 
 
