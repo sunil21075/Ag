bc9633f247b07407d7710e031723c48c7d268f3f
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2018-09-29 21:02:15 -0700

    chilling-binary_read

---
+++ Codling_moth_Code/untitled.R
@@ -0,0 +1,19 @@
+
+CodlingMothGenerations.R
+
+output = /Users/trevormozingo/Desktop/out/
+         /home/kiran/qsubs/R/output/hist/
+
+
+
+/data/hydro/users/giridhar/giridhar/codmoth_pop/alldata_us_locations/data_processed/historical
+
+Future data of WA: /data/hydro/users/giridhar/giridhar/codmoth_pop/
+
+                                                                   bcc-csm1-1-m
+                                                                   BNU-ESM
+                                                                   CanESM2
+                                                                   CNRM-CM5
+                                                                   GFDL-ESM2G
+                                                                   
+                                                                   GFDL-ESM2M
