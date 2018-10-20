d02f85561de627095729b373c400dec16c8b3947
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2018-10-19 22:15:50 -0700

    CMPOP

---
+++ Codling_moth_Code/cleaner_codes/qsub_files/local_future/CMPOP_45_qsub.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#v#!/usr/bin/env Rscript
+
+#PBS -N CMPOP_45
+#PBS -l nodes=1:ppn=1,walltime=30:00:00
+#PBS -l mem=10gb
+#PBS -q hydro
+#PBS -e CMPOP_45_error.txt
+#PBS -o CMPOP_45_output.txt
+#PBS -M h.noorazar@yahoo.com
+#PBS -m abe
+cd $PBS_O_WORKDIR
+
+# Ensure a clean running environment:
+module purge
+
+# Load modules (if needed)
+module load R/R-3.2.2_gcc
+
+/home/hnoorazar/cleaner_codes/drivers/local_future/local_future_CMPOP_45_driver.R
+
+exit 0
--- Shiny-Stuff/leaflet/leaflet6.R
+++
@@ -1,9 +0,0 @@
-
-rm(list=ls())
-library(leaflet)
-leaflet() %>%
-  addTiles() %>%
-  addRectangles(lat1 = 37.3858, lng1 = -122.0595,
-                lat2 = 37.3890, lng2 = -122.0625)
-
-
