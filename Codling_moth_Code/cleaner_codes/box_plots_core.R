52ba89bf33ef5b0268e9427fadffcb346b57ce00
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2018-11-06 22:22:29 -0800

    hey

--- Codling_moth_Code/cleaner_codes/box_plots_core.R
+++ Codling_moth_Code/cleaner_codes/box_plots_core.R
@@ -46,6 +46,35 @@ plot_some_plot <- function(input_dir, file_name, version="rcp45", plot_path, out
 }
 
 
+
+
+#data = data[, .(PercAdultGen1 = median(PercAdultGen1), 
+#                PercAdultGen2 = median(PercAdultGen2), 
+#                PercAdultGen3 = median(PercAdultGen3), 
+#                PercAdultGen4 = median(PercAdultGen4), 
+#                PercLarvaGen1 = median(PercLarvaGen1), 
+#                PercLarvaGen2 = median(PercLarvaGen2), 
+#                PercLarvaGen3 = median(PercLarvaGen3), 
+#                PercLarvaGen4 = median(PercLarvaGen4), 
+#                CumDDinC = median(CumDDinC), 
+#                CumDDinF = median(CumDDinF)), 
+#                by = c("ClimateGroup", "year", "month", "day", "dayofyear")]
+#data <- subset(data, select = c("ClimateGroup", "month", 
+#		                         "PercAdultGen1", "PercAdultGen2", "PercAdultGen3", "PercAdultGen4", 
+#		                         "PercLarvaGen1", "PercLarvaGen2", "PercLarvaGen3", "PercLarvaGen4"))
+
+#data_melted = melt(data, c("ClimateGroup", "month"), 
+#                         c("PercAdultGen1", "PercAdultGen2", "PercAdultGen3", "PercAdultGen4", 
+#                           "PercLarvaGen1", "PercLarvaGen2", "PercLarvaGen3", "PercLarvaGen4"), 
+#                         variable.name = "Generations")
+
+#p = ggplot(data = data_melted, aes(x = Generations, y = value, fill = ClimateGroup)) +
+#  geom_boxplot() + coord_flip() +
+#  facet_wrap(~month)
+
+#saveRDS(p, paste0(data_dir, "/", "popplot.rds"))
+# saveRDS(data, paste0(data_dir, "/", "subData.rds"))
+
 plot_generations_Aug23 <- function(input_dir, file_name, box_width=.25,plot_path, output_name){
 	output_name = paste0(output_name, "_", version, ".png")
 	file_name <- paste0(input_dir, file_name)
@@ -104,38 +133,3 @@ plot_generations_Aug23 <- function(input_dir, file_name, box_width=.25,plot_path
 	    coord_flip()
     ggsave(output_name, p, path=plot_path)
 }
-
-
-
-
-
-
-
-
-
-#data = data[, .(PercAdultGen1 = median(PercAdultGen1), 
-#                PercAdultGen2 = median(PercAdultGen2), 
-#                PercAdultGen3 = median(PercAdultGen3), 
-#                PercAdultGen4 = median(PercAdultGen4), 
-#                PercLarvaGen1 = median(PercLarvaGen1), 
-#                PercLarvaGen2 = median(PercLarvaGen2), 
-#                PercLarvaGen3 = median(PercLarvaGen3), 
-#                PercLarvaGen4 = median(PercLarvaGen4), 
-#                CumDDinC = median(CumDDinC), 
-#                CumDDinF = median(CumDDinF)), 
-#                by = c("ClimateGroup", "year", "month", "day", "dayofyear")]
-#data <- subset(data, select = c("ClimateGroup", "month", 
-#		                         "PercAdultGen1", "PercAdultGen2", "PercAdultGen3", "PercAdultGen4", 
-#		                         "PercLarvaGen1", "PercLarvaGen2", "PercLarvaGen3", "PercLarvaGen4"))
-
-#data_melted = melt(data, c("ClimateGroup", "month"), 
-#                         c("PercAdultGen1", "PercAdultGen2", "PercAdultGen3", "PercAdultGen4", 
-#                           "PercLarvaGen1", "PercLarvaGen2", "PercLarvaGen3", "PercLarvaGen4"), 
-#                         variable.name = "Generations")
-
-#p = ggplot(data = data_melted, aes(x = Generations, y = value, fill = ClimateGroup)) +
-#  geom_boxplot() + coord_flip() +
-#  facet_wrap(~month)
-
-#saveRDS(p, paste0(data_dir, "/", "popplot.rds"))
-# saveRDS(data, paste0(data_dir, "/", "subData.rds"))
