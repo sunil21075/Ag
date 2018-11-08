b74938be4e957f751146bb52dd4fe4fb04e9a702
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2018-11-07 20:39:33 -0800

    emergence plots

--- Codling_moth_Code/cleaner_codes/core_plot.R
+++ Codling_moth_Code/cleaner_codes/core_plot.R
@@ -45,11 +45,10 @@ plot_bloom_filling <- function(data_dir, file_name = "vertdd_combined_CMPOP_", v
   p1 = ggplot(d1, aes(x=dayofyear, y=value, fill=factor(ClimateGroup))) +
                   stat_summary(geom="ribbon", fun.y=function(z) { quantile(z,0.5) }, fun.ymin=function(z) { quantile(z,0.1) }, fun.ymax=function(z) { quantile(z,0.9) }, alpha=0.3) +
                   stat_summary(geom="ribbon", fun.y=function(z) { quantile(z,0.5) }, fun.ymin=function(z) { quantile(z,0.25) }, fun.ymax=function(z) { quantile(z,0.75) }, alpha=0.7) +
-                  stat_summary(geom="line", fun.y=function(z) { quantile(z,0.5) }, size = 1)+ #, aes(color=factor(Timeframe))) + , # aes(color=factor(ClimateGroup))
-                  scale_color_manual(values=c(rgb(29, 67, 111, max=255), rgb(92, 160, 201, max=255), rgb(211, 91, 76, max=255), rgb(125, 7, 37, max=255)))+#c("black", "red","darkgreen","blue")) +
-                  scale_fill_manual(values=c(rgb(29, 67, 111, max=255), rgb(92, 160, 201, max=255), rgb(211, 91, 76, max=255), rgb(125, 7, 37, max=255)))+#c("black", "red","darkgreen","blue")) +
+                  stat_summary(geom="line", fun.y=function(z) { quantile(z,0.5) }, size = 1)+
+                  scale_color_manual(values=c(rgb(29, 67, 111, max=255), rgb(92, 160, 201, max=255), rgb(211, 91, 76, max=255), rgb(125, 7, 37, max=255)))+
+                  scale_fill_manual(values =c(rgb(29, 67, 111, max=255), rgb(92, 160, 201, max=255), rgb(211, 91, 76, max=255), rgb(125, 7, 37, max=255)))+
                   facet_grid(. ~ variable ~ CountyGroup, scales = "free") +
-                  #xlim(45, 165) +
                   scale_x_continuous(breaks=seq(x_limits[1], x_limits[2], 10), limits = x_limits) +
                   theme_bw() +
                   labs(x = "Julian Day", y = "Proportion Full Bloom Completed", fill = "Climate Group") +
@@ -329,7 +328,7 @@ plot_generations_Aug23 <- function(input_dir,
 	                               plot_path, 
 	                               plot_output_name, 
 	                               color_ord = c("grey70", "dodgerblue", "olivedrab4", "red")
-	                               ){
+	                        ){
 	#
 	# This function does not run on Aeolus with R.3.2.2. 
 	# I will produce it on my computer.
@@ -356,7 +355,7 @@ plot_generations_Aug23 <- function(input_dir,
   box_plot = ggplot(data = data, aes(x = ClimateGroup, y = NumAdultGens, fill = ClimateGroup)) + 
              geom_boxplot(#outlier.shape = NA, 
                           outlier.size=0,
-                 notch=TRUE, width=.2) +
+                          notch=TRUE, width=.2) +
              theme_bw() +
              # The bigger the nimber in expand below, the smaller the space between y-ticks
              scale_x_discrete(expand=c(0, 3), limits = levels(data$ClimateGroup[1])) +
@@ -390,3 +389,75 @@ plot_generations_Aug23 <- function(input_dir,
 
 }
 
+
+
+plot_emergence <- function(input_dir, file_name, 
+                           box_width=.25, plot_path, output_name, 
+                           color_ord = c("grey70", "dodgerblue", "olivedrab4", "red")
+                          ){
+  #
+  # These plots are produced by combined_CM files.
+  #
+  output_name = paste0(output_name, ".png")
+  file_name <- paste0(input_dir, file_name)
+  data <- data.table(readRDS(file_name))
+  data <- subset(data, select = c("Emergence", "ClimateGroup", "ClimateScenario", 
+                                  "CountyGroup"))
+  
+  data$CountyGroup = as.character(data$CountyGroup)
+  data[CountyGroup == 1]$CountyGroup = 'Cooler Areas'
+  data[CountyGroup == 2]$CountyGroup = 'Warmer Areas'
+  
+  data = data[, .(Emergence = Emergence),
+              by = c("ClimateGroup", "CountyGroup")]
+  
+  data <- subset(data, select = c("ClimateGroup", "CountyGroup", "Emergence"))
+  
+  ######
+  ###### Compute medians of each group to annotate in the plot, if possible!!!
+  ######
+  df <- data.frame(data)
+  df <- (df %>% group_by(CountyGroup, ClimateGroup))
+  medians <- (df %>% summarise(med = median(Emergence)))
+  medians_vec <- medians$med
+  
+  p = ggplot(data = data, aes(x=ClimateGroup, y=Emergence, fill=ClimateGroup))+
+    geom_boxplot(# outlier.shape = NA, 
+      outlier.size=0, notch=TRUE, width=.2) +
+    theme_bw() +
+    scale_x_discrete(expand=c(0, 2), limits = levels(data$ClimateGroup[1])) +
+    #scale_x_discrete(limits=color_ord,limits=c(10, 20, 30)) +
+    # scale_y_discrete(limits=c(1, 2, 3, 4),labels=levels(data$ClimateGroup)) +
+    scale_y_continuous(breaks = round(seq(60, 170, by = 10),1)) +
+    labs(x="Time Period", y="Day of Year", color = "Climate Group") +
+    facet_wrap(~CountyGroup) +
+    theme(legend.position="bottom", 
+          legend.margin=margin(t = -.1, r = 0, b = 0, l = 0, unit = 'cm'),
+          # plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
+          legend.title = element_blank(),
+          panel.grid.major = element_line(size = 0.1),
+          axis.text = element_text(face = "plain", size = 10),
+          axis.title.x = element_text(face = "plain", size = 10, margin = margin(t = 5, r = 0, b = 0, l = 0)),
+          axis.text.x = element_text(size = 5),
+          axis.title.y = element_text(face = "plain", size = 10, margin = margin(t = 0, r = 1, b = 0, l = 0)),
+          axis.text.y  = element_blank(),
+          axis.ticks.y = element_blank()
+    ) +
+    scale_fill_manual(values=color_ord,
+                      name="Time\nPeriod", 
+                      labels=c("Historical","2040","2060","2080")) + 
+    scale_color_manual(values=color_ord,
+                       name="Time\nPeriod", 
+                       limits = color_ord,
+                       labels=c("Historical","2040","2060","2080")) + 
+    geom_text(data = medians, 
+              aes(label = sprintf("%1.0f", medians$med), y=medians$med), 
+              size=1.5, 
+              position =  position_dodge(.09),
+              vjust = -1.5) +
+    #stat_summary(geom="text", fun.y=quantile,
+    #             aes(label=sprintf("%1.1f", ..x..), color=factor(ClimateGroup)),
+    #             position=position_nudge(x=0.33), size=.5) +
+    coord_flip()
+  ggsave(output_name, p, path=plot_path, width=4.5, height=3.1, unit="in")
+}
