9bf27609cf1778c666e1b475d828bf2b25cf0466
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2018-12-15 20:10:41 -0800

    new_func_peak_finder_sens

--- codling_moth/code/laptop_codes/convert_csv_param_to_txt.R
+++ codling_moth/code/laptop_codes/convert_csv_param_to_txt.R
@@ -7,28 +7,33 @@ library(foreach)
 library(iterators)
 library(ggplot2)
 rm(list=ls())
-CodlingMothparameters_5_w <- read.csv("/Users/hn/Documents/GitHub/Kirti/Codling_moth_Code/sensitivity/wider_intervals/CodlingMothparameters_5.csv")
+
+write_path = "/Users/hn/Documents/GitHub/Kirti/Codling_moth_Code/sensitivity/wider_intervals/"
+read_path = "/Users/hn/Documents/GitHub/Kirti/Codling_moth_Code/sensitivity/wider_intervals/"
+
+CodlingMothparameters_5_w <- read.csv(paste0(read_path, "CodlingMothparameters_5.csv"))
 CodlingMothparameters_5_w = within(CodlingMothparameters_5_w, remove(X))
-write.table(CodlingMothparameters_5_w, "/Users/hn/Documents/GitHub/Kirti/Codling_moth_Code/sensitivity/wider_intervals/CodlingMothparameters_5_w.txt", 
+write.table(CodlingMothparameters_5_w, paste0(write_path, "CodlingMothparameters_5_w.txt"), 
             row.names = FALSE, col.names = TRUE, sep = ",")
 
 rm(list=ls())
-CodlingMothparameters_10_w <- read.csv("/Users/hn/Documents/GitHub/Kirti/Codling_moth_Code/sensitivity/wider_intervals/CodlingMothparameters_10.csv")
+CodlingMothparameters_10_w <- read.csv(paste0(read_path, "CodlingMothparameters_10.csv"))
 CodlingMothparameters_10_w = within(CodlingMothparameters_10_w, remove(X))
-write.table(CodlingMothparameters_10_w, "/Users/hn/Documents/GitHub/Kirti/Codling_moth_Code/sensitivity/wider_intervals/CodlingMothparameters_10_w.txt", 
+write.table(CodlingMothparameters_10_w, 
+	        paste0(write_path, "CodlingMothparameters_10_w.txt"), 
             row.names = FALSE, col.names = TRUE, sep = ",")
 
 
 rm(list=ls())
-CodlingMothparameters_15_w <- read.csv("/Users/hn/Documents/GitHub/Kirti/Codling_moth_Code/sensitivity/wider_intervals/CodlingMothparameters_15.csv")
+CodlingMothparameters_15_w <- read.csv(paste0(read_path, "wider_intervals/CodlingMothparameters_15.csv"))
 CodlingMothparameters_15_w = within(CodlingMothparameters_15_w, remove(X))
-write.table(CodlingMothparameters_15_w, "/Users/hn/Documents/GitHub/Kirti/Codling_moth_Code/sensitivity/wider_intervals/CodlingMothparameters_15_w.txt", 
+write.table(CodlingMothparameters_15_w, paste0(write_path, "CodlingMothparameters_15_w.txt"), 
             row.names = FALSE, col.names = TRUE, sep = ",")
 
 rm(list=ls())
-CodlingMothparameters_20_w <- read.csv("/Users/hn/Documents/GitHub/Kirti/Codling_moth_Code/sensitivity/wider_intervals/CodlingMothparameters_20.csv")
+CodlingMothparameters_20_w <- read.csv(paste0(read_path, "wider_intervals/CodlingMothparameters_20.csv"))
 CodlingMothparameters_20_w = within(CodlingMothparameters_20_w, remove(X))
-write.table(CodlingMothparameters_20_w, "/Users/hn/Documents/GitHub/Kirti/Codling_moth_Code/sensitivity/wider_intervals/CodlingMothparameters_20_w.txt", 
+write.table(CodlingMothparameters_20_w, paste0(write_path, "CodlingMothparameters_20_w.txt"), 
             row.names = FALSE, col.names = TRUE, sep = ",")
 
 
---
+++ codling_moth/code/laptop_codes/diap_sensitivity/new_funcs/escaped_diap_peaks.R
@@ -0,0 +1,76 @@
+escaped_diap_peaks <- function(file_path, file_name){
+	file_name_read = paste0(file_path, file_name)
+	data <- data.table(readRDS(file_name_read))
+	data$CountyGroup = as.character(data$CountyGroup)
+	data[CountyGroup == 1]$CountyGroup = 'Cooler Areas'
+	data[CountyGroup == 2]$CountyGroup = 'Warmer Areas'
+	
+	data$variable <- factor(data$variable)
+	if (substr(file_name, start=10, stop=12) == "abs"){
+		data = data[data$variable=="AbsNonDiap", ]
+		data <- data[variable =="AbsLarvaPop" | variable =="AbsNonDiap"]
+	    data <- subset(data, select=c("ClimateGroup", "CountyGroup", 
+		                              "CumulativeDDF", "variable", "value"))
+
+	} else {
+		data <- data[variable =="RelLarvaPop" | variable =="RelNonDiap"]
+        data <- subset(data, select=c("ClimateGroup", "CountyGroup", 
+        	                          "CumulativeDDF", "variable", "value"))
+		data = data[data$variable=="RelNonDiap", ]
+          }
+	
+	data_gen_1 <- data[data$CumulativeDDF >= 213  & data$CumulativeDDF < 1153,]
+	data_gen_2 <- data[data$CumulativeDDF >= 1153 & data$CumulativeDDF < 2313,]
+	data_gen_3 <- data[data$CumulativeDDF >= 2313 & data$CumulativeDDF < 3443,]
+	data_gen_4 <- data[data$CumulativeDDF >= 3443 & data$CumulativeDDF < 4453,]
+
+    data_gen_1_hist = data_gen_1[data_gen_1$ClimateGroup=="Historical"]
+    data_gen_1_2040 = data_gen_1[data_gen_1$ClimateGroup=="2040's"]
+    data_gen_1_2060 = data_gen_1[data_gen_1$ClimateGroup=="2060's"]
+    data_gen_1_2080 = data_gen_1[data_gen_1$ClimateGroup=="2080's"]
+
+    data_gen_2_hist = data_gen_2[data_gen_2$ClimateGroup=="Historical"]
+    data_gen_2_2040 = data_gen_2[data_gen_2$ClimateGroup=="2040's"]
+    data_gen_2_2060 = data_gen_2[data_gen_2$ClimateGroup=="2060's"]
+    data_gen_2_2080 = data_gen_2[data_gen_2$ClimateGroup=="2080's"]
+
+    data_gen_3_hist = data_gen_3[data_gen_3$ClimateGroup=="Historical"]
+    data_gen_3_2040 = data_gen_3[data_gen_3$ClimateGroup=="2040's"]
+    data_gen_3_2060 = data_gen_3[data_gen_3$ClimateGroup=="2060's"]
+    data_gen_3_2080 = data_gen_3[data_gen_3$ClimateGroup=="2080's"]
+
+    data_gen_4_hist = data_gen_4[data_gen_4$ClimateGroup=="Historical"]
+    data_gen_4_2040 = data_gen_4[data_gen_4$ClimateGroup=="2040's"]
+    data_gen_4_2060 = data_gen_4[data_gen_4$ClimateGroup=="2060's"]
+    data_gen_4_2080 = data_gen_4[data_gen_4$ClimateGroup=="2080's"]
+
+    max_table = data.table(hist = c(0, 0, 0, 0),
+    	                   "2040" = c(0, 0, 0, 0),
+    	                   "2060" = c(0, 0, 0, 0),
+    	                   "2080" = c(0, 0, 0, 0)
+    	                   )
+
+    max_table[1, "hist"] = round(max(data_gen_1_hist$value), 2)
+    max_table[2, "hist"] = round(max(data_gen_2_hist$value), 2)
+    max_table[3, "hist"] = round(max(data_gen_3_hist$value), 2)
+    max_table[4, "hist"] = round(max(data_gen_4_hist$value), 2)
+
+    max_table[1, "2040"] = round(max(data_gen_1_2040$value), 2)
+    max_table[2, "2040"] = round(max(data_gen_2_2040$value), 2)
+    max_table[3, "2040"] = round(max(data_gen_3_2040$value), 2)
+    max_table[4, "2040"] = round(max(data_gen_4_2040$value), 2)
+
+    max_table[1, "2060"] = round(max(data_gen_1_2060$value), 2)
+    max_table[2, "2060"] = round(max(data_gen_2_2060$value), 2)
+    max_table[3, "2060"] = round(max(data_gen_3_2060$value), 2)
+    max_table[4, "2060"] = round(max(data_gen_4_2060$value), 2)
+
+    max_table[1, "2080"] = round(max(data_gen_1_2080$value), 2)
+    max_table[2, "2080"] = round(max(data_gen_2_2080$value), 2)
+    max_table[3, "2080"] = round(max(data_gen_3_2080$value), 2)
+    max_table[4, "2080"] = round(max(data_gen_4_2080$value), 2)
+    write.table(max_table, paste0(file_path, substr(file_name, start=1, stop=20), "_peak", ".csv"), 
+                row.names = FALSE, col.names = TRUE, sep = ",")
+    
+}
+
