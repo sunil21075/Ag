7e132159d7ba5ff34706b93f787518f96aa3214a
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2019-01-02 21:54:22 -0800

    before_doing_window_subtraction

--- codling_moth/code/subset_4_pest/d_subset_pest_window.R
+++ codling_moth/code/subset_4_pest/d_subset_pest_window.R
@@ -7,7 +7,6 @@ output_dir = "/data/hydro/users/Hossein/codling_moth_new/local/processed/section
 name_pref = "combined_CMPOP_rcp"
 models = c("45.rds", "85.rds")
 
-
 for (model in models){
 	curr_data = readRDS(paste0(data_dir, name_pref, model))
 	curr_data = subset(curr_data, select = c(ClimateGroup, CountyGroup, CumDDinF, dayofyear, year,
