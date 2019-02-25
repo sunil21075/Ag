f162285de2cce456385a091eda42c4c8e8d2f32e
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2019-02-24 21:29:52 -0800

    analogy_worked

--- analogy/01_get_PPT/d_get_usa_observed_ppt.R
+++ analogy/01_get_PPT/d_get_usa_observed_ppt.R
@@ -6,7 +6,7 @@ library(lubridate)
 library(dplyr)
 library(data.table)
 
-source_path = "/home/hnoorazar/analog_codes/chill_core.R"
+source_path = "/home/hnoorazar/analog_codes/core_analog.R"
 source(source_path)
 
 options(digit=9)
@@ -20,6 +20,8 @@ locations_list = read.table(paste0(param_dir, "all_us_locations_list.txt"),
                             header=F, sep=",")
 locations_list <- as.vector(locations_list$V1)
 local_files = paste0("data_", locations_list)
+print ("line 23")
+print (length(local_files))
 
 # 2b. Note if working with a directory of historical data
 hist <- TRUE
@@ -32,6 +34,8 @@ if (dir.exists(main_out) == F) {
 
 # get files in current folder
 dir_con <- dir()
+print ("line 37")
+print (length(dir_con))
 
 # remove filenames that aren't data
 dir_con <- dir_con[grep(pattern = "data_",
@@ -40,8 +44,6 @@ dir_con <- dir_con[grep(pattern = "data_",
 # choose only files that we're interested in
 dir_con <- dir_con[which(dir_con %in% local_files)]
 
-print(dir_con)
-
 # 3. Process the data -----------------------------------------------------
 
 start_time <- Sys.time()
@@ -49,11 +51,16 @@ start_time <- Sys.time()
 for(file in dir_con){
 
   met_data <- read_binary(file_path = file, hist = hist, no_vars=8)
-
+  print ("line 54")
+  print (colnames(met_data))
+  print (dim(met_data))
+  
   # data frame required
   met_data <- data.table(met_data)
+  print ("line 60")
+  print (dim(met_data))
   met_data <- met_data %>%
-              select(-c(precip, tmin, SPH, SRAD, Rmax, Rmin, tmax, day, month)) %>%
+              select(-c(tmin, tmax, SPH, SRAD, Rmax, Rmin, day, month)) %>%
               data.table()
   # compute the precipitation over a year
   met_data <- aggregate(met_data$precip, by=list(year=met_data$year), FUN=sum)
@@ -61,7 +68,7 @@ for(file in dir_con){
   # rename the new column generated
   colnames(met_data)[colnames(met_data)=="x"] <- "precip"
 
-  saveRDS(met_data, paste0(main_out, current_dir, "/", file, ".rds"))
+  saveRDS(met_data, paste0(main_out, file, ".rds"))
   rm(met_data, lat, long)
 }
 
