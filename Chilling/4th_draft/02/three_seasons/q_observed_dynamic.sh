a255bd425a6f23bebc1f80714626251bfb7c2646
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2019-04-10 07:02:57 -0700

    chilling

--- Chilling/4th_draft/02/three_seasons/q_observed_dynamic.sh
+++ Chilling/4th_draft/02/three_seasons/q_observed_dynamic.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+## Export all environment variables in the qsub command's environment to the
+## batch job.
 #PBS -V
 
-#PBS -N 02_dyn_obs_chill_sea
+## Define a job name
+#PBS -N obs_dyn_chill_sea
 
-#PBS -l nodes=1:ppn=1, walltime=99:00:00
-#PBS -l mem=10gb
+## Define compute options
+#PBS -l nodes=1:ppn=1,walltime=99:00:00
+#PBS -l mem=5gb
 #PBS -q hydro
 
 #PBS -k o
   ##PBS -j oe
-#PBS -e /home/hnoorazar/chilling_codes/current_draft/02/three_seasons/error/dyn_obs_chill_sea_E
-#PBS -o /home/hnoorazar/chilling_codes/current_draft/02/three_seasons/error/dyn_obs_chill_sea_O
+#PBS -e /home/hnoorazar/chilling_codes/current_draft/02/three_seasons/error/obs_chill_sea_dyn.e
+#PBS -o /home/hnoorazar/chilling_codes/current_draft/02/three_seasons/error/obs_chill_sea_dyn.o
 
 ## Define path for reporting
 #PBS -m abe
 
 echo
-echo We are in the $PWD directory
+echo We are now in $PWD.
 echo
 
 cd /data/hydro/users/Hossein/chill/data_by_core/dynamic/01/chill_sea/observed/
@@ -34,9 +38,6 @@ module load udunits/2.2.20
 module load libxml2/2.9.4
 module load gdal/2.1.2_gcc proj/4.9.2
 module load gcc/7.3.0 r/3.5.1/gcc/7.3.0
-module load gcc/7.3.0
-module load r/3.5.1/gcc/7.3.0
-module load r/3.5.1
 
 Rscript --vanilla /home/hnoorazar/chilling_codes/current_draft/02/three_seasons/d_observed.R dynamic chill_sea
 
