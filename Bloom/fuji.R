f5eb993c5fb0775fe97aec0cde1780b3a55ac7d8
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2019-04-15 15:59:55 -0700

    all

---
+++ Bloom/fuji.R
@@ -0,0 +1,8 @@
+# Kirti,
+# The page I sent you the other day with the fruit 
+# growth models had a transcription error for Fuji 
+# - when I copied the coefficient for the squared 
+# term, I accidentally put -1.76e-08, it should have been -1.76e-07.  Its correct in this version.
+# Vince
+
+coefficient_squared_Fuji <- -1.76e-07
