ae5ddd811fcbd5b9de4ef393aad7fd2b9fb4c90b
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2019-04-15 23:52:53 -0700

    fuck me

--- Chilling/4th_draft/03_make_figures/color_code.R
+++ Chilling/4th_draft/03_make_figures/color_code.R
@@ -21,14 +21,10 @@ define_path <- function(model_name){
 }
 
 clean_process <- function(dt){
-<<<<<<< HEAD
-  dt <- subset(dt, select=c(chill_season, sum_J1, 
-                            sum_F1, sum_M1, sum_A1, lat, long,
-=======
+
   dt <- subset(dt, select=c(chill_season,
                             sum_J1, sum_F1, sum_M1, sum_A1, 
                             lat, long, climate_type,
->>>>>>> a255bd425a6f23bebc1f80714626251bfb7c2646
                             scenario, model, year))
   
   dt <- dt %>% filter(year <= 2005 | year >= 2025)
@@ -36,13 +32,8 @@ clean_process <- function(dt){
   time_periods = c("Historical", "2025_2050", "2051_2075", "2076_2099")
   dt$time_period = 0L
   dt$time_period[dt$year <= 2005] <- time_periods[1]
-<<<<<<< HEAD
   dt$time_period[dt$year >= 2025 & dt$year <= 2050] <- time_periods[2]
-  dt$time_period[dt$year >  2050 & dt$year <= 2075] <- time_periods[3]
-=======
-  dt$time_period[dt$year >= 2025 & dt$year<=2050] <- time_periods[2]
   dt$time_period[dt$year >  2050 & dt$year<=2075] <- time_periods[3]
->>>>>>> a255bd425a6f23bebc1f80714626251bfb7c2646
   dt$time_period[dt$year >  2075] <- time_periods[4]
   dt$time_period = factor(dt$time_period, levels=time_periods, order=T)
 
@@ -50,15 +41,6 @@ clean_process <- function(dt){
   dt$scenario[dt$scenario == "rcp85"] <- "RCP 8.5"
   dt$scenario[dt$scenario == "historical"] <- "Historical"
 
-<<<<<<< HEAD
-  jan_data <- subset(dt, select=c(sum_J1, lat, long, scenario, model, time_period, chill_season))
-  feb_data <- subset(dt, select=c(sum_F1, lat, long, scenario, model, time_period, chill_season))
-  mar_data <- subset(dt, select=c(sum_M1, lat, long, scenario, model, time_period, chill_season))
-  apr_data <- subset(dt, select=c(sum_A1, lat, long, scenario, model, time_period, chill_season))
-  return (list(jan_data, feb_data, mar_data, apr_data))
-}
-
-=======
   jan_data <- subset(dt, select=c(sum_J1, lat, long, climate_type, scenario, model, time_period, chill_season))
   feb_data <- subset(dt, select=c(sum_F1, lat, long, climate_type, scenario, model, time_period, chill_season))
   mar_data <- subset(dt, select=c(sum_M1, lat, long, climate_type, scenario, model, time_period, chill_season))
@@ -66,61 +48,6 @@ clean_process <- function(dt){
   return (list(jan_data, feb_data, mar_data, apr_data))
 }
 
-count_years_threshs_met <- function(dataT, due){
-  h_year_count <- length(unique(dataT[dataT$time_period =="Historical",]$chill_season))
-  f1_year_count <- length(unique(dataT[dataT$time_period== "2025_2050",]$chill_season))
-  f2_year_count <- length(unique(dataT[dataT$time_period== "2051_2075",]$chill_season))
-  f3_year_count <- length(unique(dataT[dataT$time_period== "2076_2099",]$chill_season))
-  if (due == "Jan"){
-    col_name = "sum_J1"
-    } else if (due == "Feb"){
-        col_name = "sum_F1"
-    } else if(due =="Mar"){
-        col_name = "sum_M1"
-    } else if (due =="Apr"){
-        col_name = "sum_A1"
-  }
-  
-  bks = c(-200, seq(20, 75, 5), 300)
-
-  result <- dataT %>%
-            mutate(thresh_range = cut(get(col_name), breaks = bks )) %>%
-            group_by(lat, long, climate_type, time_period, 
-                     thresh_range, model, scenario) %>%
-            summarize(no_years = n_distinct(chill_season)) %>% 
-            data.table()
-  
-  time_periods = c("Historical", "2025_2050", "2051_2075", "2076_2099")
-  result$time_period = factor(result$time_period, 
-                              levels=time_periods,
-                              order=T)
-  
-  result$thresh_range <- factor(result$thresh_range, order=T)
-  result$thresh_range <- fct_rev(result$thresh_range)
-  result <- result[order(thresh_range), ]
-
-
-  result <- result %>% 
-            group_by(lat, long, climate_type, time_period, model, scenario) %>% 
-            mutate(n_years_passed = cumsum(no_years)) %>% 
-            data.table()
-
-  # the following can be done more efficiently!
-  result_hist <- result %>% filter(time_period == "Historical") %>% data.table()
-  result_50 <- result %>% filter(time_period == "2025_2050") %>% data.table()
-  result_75 <- result %>% filter(time_period == "2051_2075") %>% data.table()
-  result_99 <- result %>% filter(time_period == "2076_2099") %>% data.table()
-  
-  result_hist$frac_passed = result_hist$n_years_passed / h_year_count
-  result_50$frac_passed = result_50$n_years_passed / f1_year_count
-  result_75$frac_passed = result_75$n_years_passed / f2_year_count
-  result_99$frac_passed = result_99$n_years_passed / f3_year_count
-
-  result <- rbind(result_hist, result_50, result_75, result_99)
-  return(result)
-}
-
->>>>>>> a255bd425a6f23bebc1f80714626251bfb7c2646
 #############################################
 ###                                       ###
 ###                 Driver                ###
@@ -140,9 +67,6 @@ main_in_dir = "/Users/hn/Desktop/Desktop/Kirti/check_point/chilling/non_overlapp
 begins <- c("mid_sept", "oct", "mid_oct", "nov", "mid_nov")
 begin = begins[1]
 mdata <- data.table(readRDS(paste0(main_in_dir, begin, ".rds")))
-
-
-
 mdata <- mdata %>% filter(model != "observed")
 information <- clean_process(mdata)
 
