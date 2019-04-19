37c9edd3b7b5e27ba8649e19a0bfab36d72c9916
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2019-04-18 22:15:34 -0700

    break_down_time_periods_analog

---
+++ analogy/03_find_analogs/R/F2/03_q_run_all_F2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /home/hnoorazar/analog_codes/03_find_analogs/rcp85_qsubs_F2/
+for runname in bcc-csm1-1-m BNU-ESM CanESM2 CNRM-CM5 GFDL-ESM2G GFDL-ESM2M
+do
+#qsub ./q_rcp85_no_precip_no_gen3_$runname.sh
+qsub ./q_rcp85_w_precip_w_gen3_$runname.sh
+qsub ./q_rcp85_no_precip_w_gen3_$runname.sh
+#qsub ./q_rcp85_w_precip_no_gen3_$runname.sh
+done
+
+cd /home/hnoorazar/analog_codes/03_find_analogs/rcp45_qsubs_F2/
+for runname in bcc-csm1-1-m BNU-ESM CanESM2 CNRM-CM5 GFDL-ESM2G GFDL-ESM2M
+do
+#qsub ./q_rcp45_no_precip_no_gen3_$runname.sh
+qsub ./q_rcp45_w_precip_w_gen3_$runname.sh
+qsub ./q_rcp45_no_precip_w_gen3_$runname.sh
+#qsub ./q_rcp45_w_precip_no_gen3_$runname.sh
+done
