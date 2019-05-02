32baf9e5553e5c5f6aeefee243cf2c2bb13c1e03
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2019-05-02 16:05:03 -0700

    hey

--- analogy/04_analysis/02_laptop/compute_statistics_of_features.R
+++ analogy/04_analysis/02_laptop/compute_statistics_of_features.R
@@ -26,6 +26,9 @@ ppp <- "/Users/hn/Documents/GitHub/Kirti/codling_moth/code/parameters/"
 min_fips <- data.table(read.csv(paste0(ppp, "us_county_lat_long.csv"), header=T, as.is=T))
 min_fips$location <- paste(min_fips$vicclat, min_fips$vicclon, sep="_")
 min_fips <- within(min_fips, remove(vicid, state_fips, vic_km2, vicclat, vicclon))
+min_fips$county = gsub("County", "", min_fips$county)
+min_fips$county <- paste0(min_fips$state, "_", min_fips$county)
+min_fips <- within(min_fips, remove(state))
 
 us_list_girid <- data.table(read.table(paste0(ppp, "all_us_locations_list.txt")))
 local_list_girid <- data.table(read.table(paste0(ppp, "local_list.txt")))
@@ -79,8 +82,8 @@ local_fips <- unique(local_cnty_fips$fips)
 us_dir <- file.path(main_dir, "usa", "/")
 local_dir <- file.path(main_dir, "local", "/")
 
-usa_cnty_fips <- within(usa_cnty_fips, remove(st_county))
-local_cnty_fips <- within(local_cnty_fips, remove(st_county))
+usa_cnty_fips <- within(usa_cnty_fips)
+local_cnty_fips <- within(local_cnty_fips)
 
 us_feat <- data.table(readRDS(paste0(us_dir, "all_data_usa.rds")))
 us_feat <- within(us_feat, remove(mean_escaped_Gen4, treatment))
@@ -152,7 +155,7 @@ cnr_F3 <- cnr %>% filter(year >= 2076)
 gfg_F3 <- gfg %>% filter(year >= 2076)
 gfm_F3 <- gfm %>% filter(year >= 2076)
 
-bcc_summary <- bcc_F3 %>%
+bcc_summary_F3 <- bcc_F3 %>%
                group_by(fips) %>%
                summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
                             vars(medianDoY, NumLarvaGens_Aug, 
@@ -160,32 +163,7 @@ bcc_summary <- bcc_F3 %>%
                                  mean_precip, mean_gdd)) %>% 
                data.table()
 
-bnu_summary <- bnu_F3 %>%
-               group_by(fips) %>%
-               summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
-                            vars(medianDoY, NumLarvaGens_Aug, 
-                                 mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
-                                 mean_precip, mean_gdd)) %>% 
-               data.table()
-
-
-can_summary <- can_F3 %>%
-               group_by(fips) %>%
-               summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
-                            vars(medianDoY, NumLarvaGens_Aug, 
-                                 mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
-                                 mean_precip, mean_gdd)) %>% 
-               data.table()
-
-cnr_summary <- cnr_F3 %>%
-               group_by(fips) %>%
-               summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
-                            vars(medianDoY, NumLarvaGens_Aug, 
-                                 mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
-                                 mean_precip, mean_gdd)) %>% 
-               data.table()
-
-gfg_summary <- gfg_F3 %>%
+bnu_summary_F3 <- bnu_F3 %>%
                group_by(fips) %>%
                summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
                             vars(medianDoY, NumLarvaGens_Aug, 
@@ -194,7 +172,23 @@ gfg_summary <- gfg_F3 %>%
                data.table()
 
 
-gfm_summary <- gfm_F3 %>%
+can_summary_F3 <- can_F3 %>%
+               group_by(fips) %>%
+               summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                            vars(medianDoY, NumLarvaGens_Aug, 
+                                 mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                 mean_precip, mean_gdd)) %>% 
+               data.table()
+
+cnr_summary_F3 <- cnr_F3 %>%
+               group_by(fips) %>%
+               summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                            vars(medianDoY, NumLarvaGens_Aug, 
+                                 mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                 mean_precip, mean_gdd)) %>% 
+               data.table()
+
+gfg_summary_F3 <- gfg_F3 %>%
                group_by(fips) %>%
                summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
                             vars(medianDoY, NumLarvaGens_Aug, 
@@ -203,9 +197,165 @@ gfm_summary <- gfm_F3 %>%
                data.table()
 
 
+gfm_summary_F3 <- gfm_F3 %>%
+               group_by(fips) %>%
+               summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                            vars(medianDoY, NumLarvaGens_Aug, 
+                                 mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                 mean_precip, mean_gdd)) %>% 
+               data.table()
+
+
+
+
+
+
+bcc_F1 <- bcc %>% filter(year <= 2050)
+bnu_F1 <- bnu %>% filter(year <= 2050)
+can_F1 <- can %>% filter(year <= 2050)
+cnr_F1 <- cnr %>% filter(year <= 2050)
+gfg_F1 <- gfg %>% filter(year <= 2050)
+gfm_F1 <- gfm %>% filter(year <= 2050)
+
+bcc_summary_F1 <- bcc_F1 %>%
+               group_by(fips) %>%
+               summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                            vars(medianDoY, NumLarvaGens_Aug, 
+                                 mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                 mean_precip, mean_gdd)) %>% 
+               data.table()
+
+bnu_summary_F1 <- bnu_F1 %>%
+               group_by(fips) %>%
+               summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                            vars(medianDoY, NumLarvaGens_Aug, 
+                                 mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                 mean_precip, mean_gdd)) %>% 
+               data.table()
+
+
+can_summary_F1 <- can_F1 %>%
+               group_by(fips) %>%
+               summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                            vars(medianDoY, NumLarvaGens_Aug, 
+                                 mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                 mean_precip, mean_gdd)) %>% 
+               data.table()
+
+cnr_summary_F1 <- cnr_F1 %>%
+               group_by(fips) %>%
+               summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                            vars(medianDoY, NumLarvaGens_Aug, 
+                                 mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                 mean_precip, mean_gdd)) %>% 
+               data.table()
+
+gfg_summary_F1 <- gfg_F1 %>%
+               group_by(fips) %>%
+               summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                            vars(medianDoY, NumLarvaGens_Aug, 
+                                 mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                 mean_precip, mean_gdd)) %>% 
+               data.table()
+
+
+gfm_summary_F1 <- gfm_F1 %>%
+               group_by(fips) %>%
+               summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                            vars(medianDoY, NumLarvaGens_Aug, 
+                                 mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                 mean_precip, mean_gdd)) %>% 
+               data.table()
+
+
+
+bcc_F2 <- bcc %>% filter(year >= 2051, year <= 2075)
+bnu_F2 <- bnu %>% filter(year >= 2051, year <= 2075)
+can_F2 <- can %>% filter(year >= 2051, year <= 2075)
+cnr_F2 <- cnr %>% filter(year >= 2051, year <= 2075)
+gfg_F2 <- gfg %>% filter(year >= 2051, year <= 2075)
+gfm_F2 <- gfm %>% filter(year >= 2051, year <= 2075)
+
+bcc_summary_F2 <- bcc_F2 %>%
+                  group_by(fips) %>%
+                  summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                               vars(medianDoY, NumLarvaGens_Aug, 
+                                    mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                    mean_precip, mean_gdd)) %>% 
+                  data.table()
+
+bnu_summary_F2 <- bnu_F2 %>%
+                  group_by(fips) %>%
+                  summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                               vars(medianDoY, NumLarvaGens_Aug, 
+                                    mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                    mean_precip, mean_gdd)) %>% 
+                  data.table()
+
+
+can_summary_F2 <- can_F2 %>%
+                     group_by(fips) %>%
+                     summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                                  vars(medianDoY, NumLarvaGens_Aug, 
+                                       mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                       mean_precip, mean_gdd)) %>% 
+                     data.table()
+
+cnr_summary_F2 <- cnr_F2 %>%
+                  group_by(fips) %>%
+                  summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                               vars(medianDoY, NumLarvaGens_Aug, 
+                                    mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                    mean_precip, mean_gdd)) %>% 
+                  data.table()
+
+gfg_summary_F2 <- gfg_F2 %>%
+                  group_by(fips) %>%
+                  summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                               vars(medianDoY, NumLarvaGens_Aug, 
+                                    mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                    mean_precip, mean_gdd)) %>% 
+                  data.table()
+
+
+gfm_summary_F2 <- gfm_F2 %>%
+                  group_by(fips) %>%
+                  summarise_at(.funs = funs(mean(., na.rm=TRUE)), 
+                               vars(medianDoY, NumLarvaGens_Aug, 
+                                    mean_escaped_Gen1, mean_escaped_Gen2, mean_escaped_Gen3,
+                                    mean_precip, mean_gdd)) %>% 
+                  data.table()
 
 
 ###############
+
+unique_fips <- min_fips
+unique_fips <- within(unique_fips, remove(location))
+unique_fips <- unique(unique_fips)
+
+bcc_summary_F1 <- left_join(bcc_summary_F1, unique_fips)
+bcc_summary_F2 <- left_join(bcc_summary_F2, unique_fips)
+bcc_summary_F3 <- left_join(bcc_summary_F3, unique_fips)
+
+new_col_names_1 <- paste0(colnames(bcc_summary_F1), "_F_1")
+new_col_names_2 <- paste0(colnames(bcc_summary_F2), "_F_2")
+new_col_names_3 <- paste0(colnames(bcc_summary_F3), "_F_3")
+
+setnames(bcc_summary_F1, old=colnames(bcc_summary_F1), new=new_col_names_1)
+setnames(bcc_summary_F2, old=colnames(bcc_summary_F2), new=new_col_names_2)
+setnames(bcc_summary_F3, old=colnames(bcc_summary_F3), new=new_col_names_3)
+
+bcc_summary <- cbind(bcc_summary_F1, bcc_summary_F2, bcc_summary_F3)
+
+helper <- setNames(data.table(matrix(nrow = (nrow(historical_summary) - nrow(bcc_summary)), 
+                                     ncol =  ncol(bcc_summary))), colnames(bcc_summary))
+
+bcc_summary <- rbind(bcc_summary, helper)
+historical_future_bcc_summary <- cbind(historical_summary, bcc_summary)
+
+write.csv(historical_future_bcc_summary, file = "/Users/hn/Desktop/historical_future_bcc_summary.csv", 
+          row.names=FALSE, na="")
+
 
 bcc_summary_chelan <- bcc_summary %>% filter(fips== 53007)
 
