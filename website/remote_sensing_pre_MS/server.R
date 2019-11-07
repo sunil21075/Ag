7d32041cd2b866983a3d8ec16344617bac5c815b
Hossein Noorazar <hn@Hosseins-MacBook-Pro.local>
2019-11-06 17:13:49 -0800

    pre_MS

--- website/remote_sensing_pre_MS/server.R
+++ website/remote_sensing_pre_MS/server.R
@@ -5,31 +5,52 @@ library(leaflet)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
 
-  output$mymap = renderLeaflet({ 
-  	  leaflet() %>%
-       addProviderTiles(providers$Esri.WorldStreetMap, # Esri.WorldStreetMap or OpenTopoMap
-                        options= providerTileOptions(opacity = 0.99))%>%
-       setView(lat = 46.5, lng = -119, zoom = 9) %>%
-       addPolygons(data = Min_sp,
-                   stroke = TRUE, 
-                   fillOpacity = 0.5, 
-                   smoothFactor = 0.5)
+  observe({
+       if (input$color == "Esri.WorldStreetMap"){
+        output$mymap = renderLeaflet({ 
+                        leaflet() %>%
+                                         # Esri.WorldStreetMap or OpenTopoMap
+                        addProviderTiles(providers$Esri.WorldStreetMap, 
+                                        options= providerTileOptions(opacity = 0.99))%>%
+                        setView(lat = 46.5, lng = -119, zoom = 9) %>%
+                        addPolygons(data = Min_sp,
+                                    stroke = TRUE, 
+                                    fillOpacity = 0.5, 
+                                   smoothFactor = 0.5)
 
-    })
+                        })
+            } else if (input$color == "OpenTopoMap"){
+             output$mymap = renderLeaflet({ 
+                            leaflet() %>%
+                                             # Esri.WorldStreetMap or OpenTopoMap
+                            addProviderTiles(providers$OpenTopoMap,
+                                            options= providerTileOptions(opacity = 0.99))%>%
+                            setView(lat = 46.5, lng = -119, zoom = 9) %>%
+                            addPolygons(data = Min_sp,
+                                        stroke = TRUE, 
+                                        fillOpacity = 0.5, 
+                                       smoothFactor = 0.5)
 
+                            })
 
-  output$mymap_1 <- renderLeaflet({
-     counties %>%
-     leaflet(options = leafletOptions(zoomControl = TRUE,
-             minZoom = 4, maxZoom = 20, dragging = TRUE))  %>%
-     addTiles() %>%
-     setView(lat = 46, lng = -119, zoom = 6) %>%
-      addPolygons(data = Min_sp,
-                   stroke = TRUE, 
-                   fillOpacity = 0.5, 
-                   smoothFactor = 0.5) %>%
-     addPolylines(data = states, color = "black", opacity = 1, weight = 1.5)
+       }
   })
+
+  
+
+
+  # output$mymap_1 <- renderLeaflet({
+  #    counties %>%
+  #    leaflet(options = leafletOptions(zoomControl = TRUE,
+  #            minZoom = 4, maxZoom = 20, dragging = TRUE))  %>%
+  #    addTiles() %>%
+  #    setView(lat = 46, lng = -119, zoom = 6) %>%
+  #     addPolygons(data = Min_sp,
+  #                  stroke = TRUE, 
+  #                  fillOpacity = 0.5, 
+  #                  smoothFactor = 0.5) %>%
+  #    addPolylines(data = states, color = "black", opacity = 1, weight = 1.5)
+  # })
            
 })
 
--- website/remote_sensing_pre_MS/ui.R
+++ website/remote_sensing_pre_MS/ui.R
@@ -1,11 +1,36 @@
-rm(list=ls())
-library(shiny)
-library(leaflet)
-r_colors <- rgb(t(col2rgb(colors()) / 255))
-names(r_colors) <- colors()
+# Per_MS Meeting
 
+vars <- c("Esri.WorldStreetMap", "OpenTopoMap")
 
-shinyUI(
-	fluidPage(leafletOutput("mymap")
-              )
+navbarPage("Pre-MS", id="nav",
+
+  tabPanel("Map of polygons of double crops filtered by Notes and RotationCrop",
+    div(class="outer",
+
+      tags$head(
+        # Include CSS
+        includeCSS("styles.css"),
+        includeScript("gomap.js")
+      ),
+
+      # If not using custom CSS, set height of leafletOutput to a number instead of percent
+      leafletOutput("mymap", width="100%", height="100%"),
+      absolutePanel(id = "controls", 
+                    class = "panel panel-default", fixed = TRUE,
+                    draggable = TRUE, top = 60, 
+                    left = "auto", right = 20, bottom = "auto",
+                    width = 330, height = "auto",
+
+                    h2("Tile Choice"),
+                    selectInput("color", label=" ", 
+                                choices=vars,
+                                selected = vars[1])
+                  ),
+
+      tags$div(id="cite",
+               'Data compiled for ', tags$em('Coming Apart: The State of White America, 1960–2010'), ' by Charles Murray (Crown Forum, 2012).'
+               )
+
+    )
+  )
 )
