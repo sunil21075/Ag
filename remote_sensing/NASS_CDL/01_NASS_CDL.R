70a030634883871bf19912cdc607ede14b4939f3
Hossein Noorazar <hn@orcpos01.ad.wsu.edu>
2020-07-24 09:08:43 -0700

    remove outliers for allcounties

--- remote_sensing/NASS_CDL/01_NASS_CDL.R
+++ remote_sensing/NASS_CDL/01_NASS_CDL.R
@@ -8,12 +8,45 @@ library(sp)
 
 download_dir <- paste0("/Users/hn/Documents/01_research_data/", 
                        "remote_sensing/NASS_CDL/download_by_cdlTools/")
+
 WA_2019 <- cdlTools::getCDL(x = 'Washington', year = 2019, location = download_dir, ssl.verifypeer = FALSE)
+WA_2018 <- cdlTools::getCDL(x = 'Washington', year = 2018, location = download_dir, ssl.verifypeer = FALSE)
+WA_2017 <- cdlTools::getCDL(x = 'Washington', year = 2017, location = download_dir, ssl.verifypeer = FALSE)
 
 # you can get counts with table
+WA_2019_table <- table(raster::getValues(WA_2019[[1]]))
 WA_2018_table <- table(raster::getValues(WA_2018[[1]]))
-WA_2018_table
+WA_2017_table <- table(raster::getValues(WA_2017[[1]]))
 
-crop_count <- data.frame( crop = updateNamesCDL(names(WA_2018_table)), 
-                          WA_2018_table , 
-                          stringsAsFactors=FALSE)
+
+crop_count_2019 <- data.frame( crop = updateNamesCDL(names(WA_2019_table)), 
+                               WA_2019_table , 
+                               stringsAsFactors=FALSE)
+
+crop_count_2018 <- data.frame( crop = updateNamesCDL(names(WA_2018_table)), 
+                               WA_2018_table , 
+                               stringsAsFactors=FALSE)
+
+crop_count_2017 <- data.frame( crop = updateNamesCDL(names(WA_2017_table)), 
+                               WA_2017_table , 
+                               stringsAsFactors=FALSE)
+
+
+crop_count_2019$crop <- tolower(crop_count_2019$crop)
+crop_count_2018$crop <- tolower(crop_count_2018$crop)
+crop_count_2017$crop <- tolower(crop_count_2017$crop)
+
+crop_count_2019 <- crop_count_2019[grepl('dbl', crop_count_2019$crop), ]
+crop_count_2018 <- crop_count_2018[grepl('dbl', crop_count_2018$crop), ]
+crop_count_2017 <- crop_count_2017[grepl('dbl', crop_count_2017$crop), ]
+
+
+crop_count_2019
+crop_count_2018
+crop_count_2017
+
+sum(crop_count_2019$Freq)
+sum(crop_count_2018$Freq)
+sum(crop_count_2017$Freq)
+
+
---
+++ remote_sensing/python/drivers/04_remove_outliers_n_jumps/Grant_2017_2Yrs/00_2Yrs_Grant_2017_remove_outliers.py
@@ -0,0 +1,173 @@
+####
+#### July 23, 2020
+####
+
+"""
+  remove outliers that are beyond -1 and 1 in NDVI and EVI.
+  Looking at 2017 data I did not see any NDVI beyond those boundaries. 
+  EVI had outliers only.
+"""
+
+import csv
+import numpy as np
+import pandas as pd
+# import geopandas as gpd
+from IPython.display import Image
+# from shapely.geometry import Point, Polygon
+from math import factorial
+import scipy
+import scipy.signal
+import os, os.path
+
+from datetime import date
+import datetime
+import time
+
+from statsmodels.sandbox.regression.predstd import wls_prediction_std
+from sklearn.linear_model import LinearRegression
+from patsy import cr
+
+# from pprint import pprint
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+import sys
+start_time = time.time()
+
+# search path for modules
+# look @ https://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path
+
+
+####################################################################################
+###
+###                      Local
+###
+####################################################################################
+
+###
+### Core path
+###
+
+# sys.path.append('/Users/hn/Documents/00_GitHub/Ag/remote_sensing/python/')
+
+###
+### Directories
+###
+
+# param_dir = "/Users/hn/Documents/00_GitHub/Ag/remote_sensing/parameters/"
+####################################################################################
+###
+###                      Aeolus Core path
+###
+####################################################################################
+
+sys.path.append('/home/hnoorazar/remote_sensing_codes/')
+
+####################################################################################
+###
+###                   Aeolus Directories
+###
+####################################################################################
+
+data_dir = "/data/hydro/users/Hossein/remote_sensing/02_Eastern_WA_EE_TS/2Years/"
+param_dir = "/home/hnoorazar/remote_sensing_codes/parameters/"
+
+####################################################################################
+###
+###                   Import remote cores
+###
+####################################################################################
+
+import remote_sensing_core as rc
+import remote_sensing_core as rcp
+
+####################################################################################
+###
+###      Parameters                   
+###
+####################################################################################
+
+indeks = sys.argv[1]
+# county = "Grant"
+SF_year = 2017
+regular_window_size = 10
+########################################################################################
+###
+###                   process data
+###
+########################################################################################
+
+f_name = "Eastern_WA_" + str(SF_year) + "_70cloud_selectors.csv"
+an_EE_TS = pd.read_csv(data_dir + f_name, low_memory=False)
+
+########################################################################################
+
+# an_EE_TS = an_EE_TS[an_EE_TS['county'] == county] # Filter Grant
+an_EE_TS['SF_year'] = SF_year
+
+########################################################################################
+
+output_dir = data_dir + "/outliers_removed/"
+os.makedirs(output_dir, exist_ok=True)
+########################################################################################
+
+if (indeks == "EVI"):
+    an_EE_TS = rc.initial_clean_EVI(an_EE_TS)
+else:
+    an_EE_TS = rc.initial_clean_NDVI(an_EE_TS)
+
+an_EE_TS.head(2)
+
+###
+### List of unique polygons
+###
+polygon_list = an_EE_TS['ID'].unique()
+print(len(polygon_list))
+
+########################################################################################
+
+output_df = pd.DataFrame(data = None,
+                         index = np.arange(an_EE_TS.shape[0]), 
+                         columns = an_EE_TS.columns)
+
+counter = 0
+row_pointer = 0
+
+for a_poly in polygon_list:
+    if (counter % 300 == 0):
+        print (counter)
+    curr_field = an_EE_TS[an_EE_TS['ID']==a_poly].copy()
+    ################################################################
+    # Sort by DoY (sanitary check)
+    curr_field.sort_values(by=['image_year', 'doy'], inplace=True)
+    curr_field.reset_index(drop=True, inplace=True)
+    
+    # print ("print(curr_field.shape")
+    # print(curr_field.shape)
+    # print ("__________________________________________")
+    ################################################################
+    no_Outlier_TS = rc.interpolate_outliers_EVI_NDVI(outlier_input = curr_field, given_col = indeks)
+
+    output_df[row_pointer: row_pointer + curr_field.shape[0]] = no_Outlier_TS.values
+    counter += 1
+    row_pointer += curr_field.shape[0]
+
+
+####################################################################################
+###
+###                   Write the outputs
+###
+####################################################################################
+                                       # county 
+out_name = output_dir + "00_noOutlier_" + "_SF_" + str(SF_year) + "_" + indeks + ".csv"
+
+os.makedirs(output_dir, exist_ok=True)
+output_df.to_csv(out_name, index = False)
+
+end_time = time.time()
+print(end_time - start_time)
+
+
+
+
+
